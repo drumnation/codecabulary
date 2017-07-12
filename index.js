2e1b990b9ec519fe58e6df84c14832b7049f5705
drumnation <davidmieloch@gmail.com>
2017-07-12 18:16:28 -0400

    uncommited changes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http')
+var express = require('express')
+var router = express.Router()
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"})
-  response.end("Hello World\n")
-}).listen(process.env.PORT)
+  response.end()
+}).listen(process.env.PORT || 3000)
+
+router.get('/', function (req, res) {
+  res.send('hello world')
+})
